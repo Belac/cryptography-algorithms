1f1822252c0d47d2462fb2e16c774e169a1d50e7
Belac <94429901+Belac@users.noreply.github.com>
2022-03-08 18:17:43 +0000

    Add files via upload

---
+++ code_affine.py
@@ -0,0 +1,29 @@
+from math import gcd
+import string
+
+def phi(n):
+    value = 0
+    for i in range(1, n):
+        value += gcd(i, n) == 1
+    return value
+
+def cipher(txt, a, b):
+    txt = txt.upper()
+    letters = string.ascii_uppercase
+    _txt = ''
+    for char in txt:
+        if char.isalpha():
+            _txt += letters[(a * letters.index(char) + b) % 26]
+        else:
+            _txt += char
+    return _txt
+
+def decipher(txt, a, b):
+    for i in range(1, 26):
+        if (a * i) % 26 == 1:
+            a = i
+            b *= -a
+            break
+    return cipher(txt, a, b)
+
+print(decipher(cipher("YOUR MOTHER IS A B*TCH", 7, 10), 7, 10))
