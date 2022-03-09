a467b28c986f9afa28fb39b8da03184c1a480e25
Belac <94429901+Belac@users.noreply.github.com>
2022-03-09 21:35:53 +0000

    Update vigenere_code.py

--- vigenere_code.py
+++ vigenere_code.py
@@ -1,46 +1,38 @@
 import string
 
-def cipher(txt, word):
-    """We are in base 26 (English alphabets).
-    This function is set to cipher your text through the
-    key that's a simple word"""
-    txt = txt.upper()
+cap_letters = string.ascii_uppercase
+tin_letters = string.ascii_lowercase
+
+
+def cipher(text, word):
+    _text = ''
     word = word.upper()
-    upper_letters = string.ascii_uppercase
-    encrypted_txt = ''
     j = 0
-    for i in range(len(txt)):
-        if txt[i].isupper():
-            encrypted_txt += upper_letters[(upper_letters.index(txt[i]) + upper_letters.find(word[j])) % 26]
+    for i in range(len(text)):
+        if text[i] in cap_letters:
+            _text += cap_letters[(cap_letters.index(text[i]) + cap_letters.find(word[j])) % 26]
+            j += 1
+        elif text[i] in tin_letters:
+            _text += tin_letters[(tin_letters.index(text[i]) + cap_letters.find(word[j])) % 26]
             j += 1
         else:
-            encrypted_txt += txt[i]
-        if j == len(word):
-            j = 0
-    return encrypted_txt
+            _text += text[i]
+        j %= len(word)
+    return _text
 
-"""def decipher(txt, word):
-        #We can decipher an encrypted text by ciphering it twenty-six times
-        for i in range(25):
-            txt = cipher(txt, word)
-    return txt"""
 
-def decipher(txt, word):
-    txt = txt.upper()
+def decipher(text, word):
+    _text = ''
     word = word.upper()
-    upper_letters = string.ascii_uppercase
-    decrypted_txt = ''
     j = 0
-    for i in range(len(txt)):
-        if txt[i].isupper():
-            decrypted_txt += upper_letters[(upper_letters.index(txt[i]) - upper_letters.find(word[j])) % 26]
+    for i in range(len(text)):
+        if text[i] in cap_letters:
+            _text += cap_letters[(cap_letters.index(text[i]) - cap_letters.find(word[j])) % 26]
+            j += 1
+        elif text[i] in tin_letters:
+            _text += tin_letters[(tin_letters.index(text[i]) - cap_letters.find(word[j])) % 26]
             j += 1
         else:
-            decrypted_txt += txt[i]
-        if j == len(word):
-            j = 0
-    return decrypted_txt
-
-#print(decipher(cipher("CETTEPHRASENEVEUTRIENDIRE", 'RIDE'), 'RIDE'))
-    
-    
+            _text += text[i]
+        j %= len(word)
+    return _text
